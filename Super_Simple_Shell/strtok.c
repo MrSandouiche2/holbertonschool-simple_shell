47a2b9e1b3bac749e13208ea0a27207fe62a88f1
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-14 11:42:35 +0200

    mod file for strtok

--- Super_Simple_Shell/strtok.c
+++ Super_Simple_Shell/strtok.c
@@ -7,14 +7,18 @@ char **split_string(char *str);
 int main(void)
 {
 
-
-char *chaine;
 char **vide;
 vide = NULL;
 int compteur;
-chaine = "valeur par defaut";
+char chaine[] = "Bonjour à tous Laval";
 
 vide = split_string(chaine);
+
+if (vide == NULL)
+{
+	printf("Aucun mot trouvé.\n");
+	return (1);
+}
 
 for (compteur = 0; vide[compteur] != NULL; compteur++)
 {
@@ -31,7 +35,45 @@ return (0);
 }
 char **split_string(char *str)
 {
-	char **tableau;
-	char *mot;
-	int nombre_de_mots = 0;
-}	int compteur = 0;
+	int initial_size = 10; /*alloue de la mémoire pour 10 mots*/
+	char **tableau = malloc(sizeof(char *) * initial_size);
+	int compteur = 0;
+
+	/*Vérification de l'argument*/
+	if (str == NULL || strlen(str) == 0)
+		{
+			return (NULL);
+		}
+
+	const char *delimiteurs = " ,;!?\n";
+
+	char *mot = strtok(str, delimiteurs);
+	while (mot != NULL)/*Vérifier si la mémoire est suffisante*/
+		{
+			if (compteur >= initial_size)
+			{
+				initial_size *= 2;/*double la taille*/
+				tableau = realloc(tableau, sizeof(char*) * initial_size);
+				if (tableau == NULL)
+				{
+					return (NULL);
+				}
+			}
+			/*allocation pour le mot*/
+			tableau[compteur] = malloc(strlen(mot) + 1);
+			if (tableau[compteur] == NULL)
+			{
+				return (NULL);
+			}
+			strcpy(tableau[compteur], mot);/*copie le mot*/
+			compteur++;/*incrémente le compteur*/
+
+			/*recupere le mot suivant*/
+			mot = strtok(NULL, delimiteurs);
+		}
+	/*terminer le tableau avec NULL*/
+		tableau[compteur] = NULL;
+
+	return (tableau);
+}
+
