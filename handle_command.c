1e33b4886f58b0650574669d03bb7f2db15007f3
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-08-19 10:06:58 +0200

    updated handle commands

--- handle_command.c
+++ handle_command.c
@@ -9,7 +9,7 @@
  * processes it accordingly. If it is an external command, it
  * forks a new process to execute it.
  */
-void handle_command(char *command, char *prog_name)
+void handle_command(char *command)
 {
 	pid_t pid;
 	int status;
@@ -20,29 +20,22 @@ void handle_command(char *command, char *prog_name)
 	}
 	else
 	{
-		/* Vérifier qu'il n'y a pas d'espaces dans la commande */
-		if (strchr(command, ' ') != NULL)
-		{
-			printf("%s: Command should not contain any arguments.\n", prog_name);
-			return; /* Ne pas exécuter la commande si des arguments sont présents */
-		}
-
 		pid = fork();
 		if (pid == 0)
 		{
 			char *argv[2];
-			char *envp[] = { NULL }; /* Passer un environnement vide */
-
-			argv[0] = command; /* Commande unique sans arguments */
+			argv[0] = command;
 			argv[1] = NULL;
 
-			execve(argv[0], argv, envp);
-			fprintf(stderr, "%s: 1: %s: not found\n", prog_name, command);
-			exit(1);
+			if (execve(command, argv, environ) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
 		}
 		else if (pid < 0)
 		{
-			perror(prog_name);
+			perror("fork");
 		}
 		else
 		{
@@ -50,4 +43,3 @@ void handle_command(char *command, char *prog_name)
 		}
 	}
 }
-
