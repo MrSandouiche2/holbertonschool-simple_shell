89dffac0586d1ec11c7eb62474753a0396faa727
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-19 09:35:56 +0200

    mod file handle_command for problem checker

--- handle_command.c
+++ handle_command.c
@@ -1,36 +1,32 @@
 #include "header.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
-/**
- * handle_command - Handle external commands.
- * @command: The command to handle.
- *
- * This function checks if the command is a built-in command and
- * processes it accordingly. If it is an external command, it
- * forks a new process to execute it.
- */
+/* Dans la fonction handle_command, remplacer execvp par execve */
 void handle_command(char *command)
 {
+	pid_t pid;
+	int status;
+
 	if (is_builtin(command))
 	{
 		handle_builtins(command);
 	}
 	else
 	{
-		pid_t pid;
-		int status;
-
-		pid = fork();/*Déplace l'affectation hors du if*/
+		pid = fork();
 		if (pid == 0)
 		{
-			char *argv[4];
+			char *argv[2];
+			char *envp[] = { NULL }; /* Passer un environnement vide */
 
-			argv[0] = "/bin/sh";
-			argv[1] = "-c";
-			argv[2] = command;
-			argv[3] = NULL;
+			argv[0] = command;/* Commande unique sans arguments */
+			argv[1] = NULL;
 
-			execvp(argv[0], argv);
-			perror("Command not found");
+			execve(argv[0], argv, envp);
+			perror(argv[0]);
 			exit(1);
 		}
 		else if (pid < 0)
@@ -43,5 +39,3 @@ void handle_command(char *command)
 		}
 	}
 }
-
-
